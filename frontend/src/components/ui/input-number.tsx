3a54db4a9f2744546052c28ca427fb189299ea31
Berk Selvi <berkslv@gmail.com>
2023-10-13 14:42:45 +0300

    PLN-58 projeler ekranı tamamlandı

---
+++ frontend/src/components/ui/input-number.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FormControl, FormField, FormItem, FormMessage } from "./form";
+import { Input } from "./input";
+import { Control } from "react-hook-form";
+
+interface InputNumberProps extends React.HTMLAttributes<HTMLDivElement> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  control: Control<any>;
+  placeholder: string;
+  fieldName: string;
+}
+
+const InputNumber = ({ control, placeholder, fieldName } : InputNumberProps) => {
+  return (
+    <FormField
+      control={control}
+      name={fieldName}
+      render={({ field }) => (
+        <FormItem>
+          <FormControl>
+            <Input type="number" placeholder={placeholder} {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default InputNumber;
